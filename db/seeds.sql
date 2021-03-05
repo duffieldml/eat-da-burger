836550fe06d69f8a8d413a18d8e9edb85b6ed555
Matt Duffield <duffieldml@gmail.com>
2021-03-04 19:43:07 -0500

    Seeds are ready for database

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,5 @@
 -- Need to populate with at least 3 burgers
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Western Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hambuger', true);
